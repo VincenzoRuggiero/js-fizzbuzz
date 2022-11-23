a0c7b74183fb3188507e5f8164e698d20a1550b5
VincenzoRuggiero <vincenzoru@live.it>
2022-11-23 16:48:24 +0100

    Correction

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
-const containerElem = document.querySelector(".container");
-
 for (i = 1; i <= 100; i++) {
-  console.log(i);
-
-  if (i % 3 === 0) {
-    containerElem.innerHTML += `<p class="fizz">Fizz</p>`;
-
-    if (i % 5 === 0) {
-      containerElem.innerHTML += `<p class="buzz">Buzz</p>`;
-    }
-
-    if (i % 3 === 0 && i % 5 === 0) {
-      containerElem.innerHTML += `<p class="fizzbuzz">FizzBuzz</p>`;
-    }
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i + " FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log(i + " Fizz");
+  } else if (i % 5 === 0) {
+    console.log(i + " Buzz");
+  } else {
+    console.log(i);
   }
 }
